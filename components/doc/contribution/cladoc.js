566fd39e5ee1ab22c78e42aa0d64f36a6a31d383
Sven Jansen <sja@cslab.de>
2026-07-06 11:34:50 +0200

    Rebranded leftovers

--- components/doc/contribution/cladoc.js
+++ components/doc/contribution/cladoc.js
@@ -3,7 +3,7 @@ import { DocSectionText } from '@/components/doc/common/docsectiontext';
 export function CLADoc(props) {
     return (
         <DocSectionText {...props}>
-            <p>When a community member is offered the Contributor role, they are expected to sign a Contributor License Agreement (CLA) for legal purposes. This helps protect both the contributor and PrimeTek.</p>
+            <p>Mantle UI does not currently require a separate Contributor License Agreement. Contributions are handled under the repository contribution policy and the project license.</p>
         </DocSectionText>
     );
 }
